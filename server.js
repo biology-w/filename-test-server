b32ee48ade3b3d25ce8a904ebaacaa91b912f686
wjunw <1185279859@qq.com>
2023-02-24 20:00:39 +0800

    init

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path")
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -10,36 +10,35 @@ app.all("*", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("Content-Type", "application/json;charset=utf-8");
-  if (req.method.toLowerCase() == "options") res.sendStatus(200);
-  else {
+  if (req.method.toLowerCase() == "options") {
+    res.sendStatus(200);
+  } else {
     next();
   }
 });
 
-app.get("/download",(req,res) => {
-    try {
-        const filepath = __dirname + "/public/债券承销排行榜.pdf";
-        fs.readFile(filepath,(err,data) => {
-            // res.writeHead(200, {
-            //   "Content-Type": "application/octet-stream",
-            //   "Content-Disposition": `attachment; filename*=UTF-8''${encodeURIComponent(
-            //     "债券承销排行榜.pdf"
-            //   )}`,
-            //   "Content-Length": 308716,
-            // });
-            res.writeHead(200, {
-              "Content-Type": "application/octet-stream",
-              "Content-Disposition": `attachment; filename=${encodeURIComponent(
-                "债券承销排行榜.pdf"
-              )}`,
-              "Content-Length": 308716
-            });
-            res.end(data)
-        })
-    } catch (error) {
-        return res.status(500).send(error)
-    }
-})
+app.get("/download", (req, res) => {
+  try {
+    const filepath = __dirname + "/public/测试编码是否正常.pdf";
+    fs.readFile(filepath, (err, data) => {
+        // res.writeHead(200, {
+        //   "Content-Type": "application/octet-stream",
+        //   "Content-Disposition": `attachment; filename*=UTF-8''${encodeURIComponent(
+        //     "测试编码是否正常.pdf"
+        //   )}`,
+        // });
+      res.writeHead(200, {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": `attachment; filename=${encodeURIComponent(
+          "测试编码是否正常.pdf"
+        )}`,
+      });
+      res.end(data);
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
 
 app.listen(8001, () => {
   console.log("http://localhost:8001 is running");
